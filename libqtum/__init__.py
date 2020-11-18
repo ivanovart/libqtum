2ce1f704d0fb2b70bbce7efdc2c168fe62ee68d7
Artem Ivanov <artem@worklife.io>
2020-11-18 17:13:49 +0100

    Add address validation function

--- libqtum/__init__.py
+++ libqtum/__init__.py
@@ -1,6 +1,6 @@
 """Python library to work with QTUM Blockchain"""
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 from .key import PrivateKey, PublicKey
 from .network import QtumMainNet, QtumTestNet
--- libqtum/address.py
+++ libqtum/address.py
@@ -1,6 +1,24 @@
-from .base58 import b58decode
+from .base58 import b58decode, b58encode
+from .network import Network
+from .utils import hash256
 
 
 def base58_address_to_hex(address: str) -> bytes:
     raw = b58decode(address)
     return raw[1:-4]
+
+
+def hex_address_to_base58(address: bytes, network: Network) -> str:
+    # Prepend the network address byte
+    network_hash160_bytes = network.PUBKEY_ADDRESS.to_bytes(1, "big") + address
+    # Return a base58 encoded address with a checksum
+    checksum = hash256(network_hash160_bytes)
+    return b58encode(network_hash160_bytes + checksum[:4]).decode("utf-8")
+
+
+def base58_address_validate(address: str, network: Network) -> bool:
+    raw = b58decode(address)
+    return network.PUBKEY_ADDRESS.to_bytes(1, "big") == raw[:1] and (
+        hash256(network.PUBKEY_ADDRESS.to_bytes(1, "big") + raw[1:-4])[:4]
+        == raw[-4:]
+    )
